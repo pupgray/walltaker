dee3dbf654892f5de598627a42f251c8e0e61932
Pup Gray <pupgray@outlook.com>
2024-06-28 17:44:38 -0400

    hide this option when not logged in

--- app/controllers/link_lists_controller.rb
+++ app/controllers/link_lists_controller.rb
@@ -1,5 +1,7 @@
 class LinkListsController < ApplicationController
   def index
+    return redirect_to link_lists_path if current_user.nil? && params[:filter] == 'mine'
+
     query = Kink.joins(:kink_havers, :links).where(links: Link.is_public).having('count(links) > 0').group(:id).order('count(kink_havers) desc') if params[:filter].nil?
     query = Kink.joins(:kink_havers, :links).where(links: Link.is_public, id: current_user.kinks.pluck(:id)).having('count(links) > 0').group(:id).order('count(kink_havers) desc') if params[:filter] == 'mine'
     @pagy, @kinks = pagy(query, items: 5)
