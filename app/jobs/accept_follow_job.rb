5cbd751e6d2f1d4eb6eb044f18d3c65765d8adab
Pup Gray <pupgray@outlook.com>
2024-07-14 23:28:36 -0400

    path heleprs heuhiudhfaksjdhfakjsdhf

--- app/jobs/accept_follow_job.rb
+++ app/jobs/accept_follow_job.rb
@@ -1,7 +1,7 @@
 class AcceptFollowJob < ApplicationJob
   queue_as :default
 
-  def perform(actor, id, user)
+  def perform(actor, id, user,  key_path)
     actor_response = Excon.get(actor, headers: { 'Accept': 'application/activity+json' })
     inbox = JSON.parse(actor_response.body)['inbox']
     document = JSON.dump({
@@ -16,7 +16,7 @@ class AcceptFollowJob < ApplicationJob
     keypair = OpenSSL::PKey::RSA.new(Key.find_by_purpose(:activity_pub).private)
     signed_string = "(request-target): post /inbox\nhost: mastodon.social\ndate: #{date}\ndigest: #{digest}"
     signature = Base64.strict_encode64(keypair.sign(OpenSSL::Digest::SHA256.new, signed_string))
-    header = 'keyId="' + actor_url(user.username, anchor: 'main-key') + '",headers="(request-target) host date digest",signature="' + signature + '"'
+    header = 'keyId="' + key_path + '",headers="(request-target) host date digest",signature="' + signature + '"'
     result = Excon.post(inbox, body: document, headers: { 'Content-Type': 'application/activity+json', 'Host': URI.parse(inbox).host, 'Date': date, 'Signature': header, 'Digest': digest })
     logger.fatal "GUH!!!!" + result.body
   end
