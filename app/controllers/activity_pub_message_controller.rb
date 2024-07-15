b43d5361008068045bc11db15faf10ce57c564a2
Pup Gray <pupgray@outlook.com>
2024-07-14 23:23:33 -0400

    maybe? too fast i guess

--- app/controllers/activity_pub_message_controller.rb
+++ app/controllers/activity_pub_message_controller.rb
@@ -11,35 +11,10 @@ class ActivityPubMessageController < ApplicationController
     case type
     when 'Follow'
       ApFollower.create(user:, url: actor)
-      logger.debug "GUH! #{request.raw_post}"
-
-      actor_response = Excon.get(actor, headers: { 'Accept': 'application/activity+json' })
-      logger.debug "GUH!2 #{actor_response.body}"
-      inbox = JSON.parse(actor_response.body)['inbox']
-      document = JSON.dump({
-                             "@context": "https://www.w3.org/ns/activitystreams",
-                             "type": "Accept",
-                             "actor": user,
-                             "object": id
-                           })
-      logger.debug "GUH!3 #{document}"
-      sha256 = OpenSSL::Digest::SHA256.new
-      digest = "SHA-256=" + Base64.strict_encode64(sha256.digest(document))
-      date = Time.now.utc.httpdate
-      logger.debug "GUH!4 #{digest} ---- #{date}"
-      keypair = OpenSSL::PKey::RSA.new(Key.find_by_purpose(:activity_pub).private)
-      signed_string = "(request-target): post /inbox\nhost: mastodon.social\ndate: #{date}\ndigest: #{digest}"
-      logger.debug "GUH!5 #{signed_string}"
-      signature = Base64.strict_encode64(keypair.sign(OpenSSL::Digest::SHA256.new, signed_string))
-      logger.info "GUH!6 #{inbox} #{actor_response.body}"
-      header = 'keyId="' + actor_url(user.username, anchor: 'main-key') + '",headers="(request-target) host date digest",signature="' + signature + '"'
-      logger.fatal "GUH!7 #{header}"
-
-      result = Excon.post(inbox, body: document, headers: { 'Content-Type': 'application/activity+json', 'Host': URI.parse(inbox).host, 'Date': date, 'Signature': header, 'Digest': digest })
-      logger.info "GUH!8 #{result.status} #{result.body}"
-
-      render content_type: 'application/activity+json'
+      AcceptFollowJob.perform_later(actor, id, user)
     end
+
+    render content_type: 'application/activity+json'
   end
 
   private
---
+++ app/jobs/accept_follow_job.rb
@@ -0,0 +1,23 @@
+class AcceptFollowJob < ApplicationJob
+  queue_as :default
+
+  def perform(actor, id, user)
+    actor_response = Excon.get(actor, headers: { 'Accept': 'application/activity+json' })
+    inbox = JSON.parse(actor_response.body)['inbox']
+    document = JSON.dump({
+                           "@context": "https://www.w3.org/ns/activitystreams",
+                           "type": "Accept",
+                           "actor": user,
+                           "object": id
+                         })
+    sha256 = OpenSSL::Digest::SHA256.new
+    digest = "SHA-256=" + Base64.strict_encode64(sha256.digest(document))
+    date = Time.now.utc.httpdate
+    keypair = OpenSSL::PKey::RSA.new(Key.find_by_purpose(:activity_pub).private)
+    signed_string = "(request-target): post /inbox\nhost: mastodon.social\ndate: #{date}\ndigest: #{digest}"
+    signature = Base64.strict_encode64(keypair.sign(OpenSSL::Digest::SHA256.new, signed_string))
+    header = 'keyId="' + actor_url(user.username, anchor: 'main-key') + '",headers="(request-target) host date digest",signature="' + signature + '"'
+    result = Excon.post(inbox, body: document, headers: { 'Content-Type': 'application/activity+json', 'Host': URI.parse(inbox).host, 'Date': date, 'Signature': header, 'Digest': digest })
+    logger.fatal "GUH!!!!" + result.body
+  end
+end
