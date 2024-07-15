55dd2438c570dc1343d222929e4021d6dfe4008e
Pup Gray <pupgray@outlook.com>
2024-07-14 22:25:13 -0400

    guh2

--- app/controllers/activity_pub_message_controller.rb
+++ app/controllers/activity_pub_message_controller.rb
@@ -11,7 +11,10 @@ class ActivityPubMessageController < ApplicationController
     case type
     when 'Follow'
       ApFollower.create(user:, url: actor)
+      logger.debug "GUH! #{request.raw_post}"
+
       actor_response = Excon.get(actor, headers: { 'Accept': 'application/jrd+json' })
+      logger.debug "GUH!2 #{actor_response.body}"
       inbox = JSON.parse(actor_response.body)['inbox']
       document = JSON.dump({
                              "@context": "https://www.w3.org/ns/activitystreams",
@@ -19,17 +22,21 @@ class ActivityPubMessageController < ApplicationController
                              "actor": user,
                              "object": id
                            })
+      logger.debug "GUH!3 #{document}"
       sha256 = OpenSSL::Digest::SHA256.new
       digest = "SHA-256=" + Base64.strict_encode64(sha256.digest(document))
       date = Time.now.utc.httpdate
+      logger.debug "GUH!4 #{digest} ---- #{date}"
       keypair = OpenSSL::PKey::RSA.new(Key.find_by_purpose(:activity_pub).private)
       signed_string = "(request-target): post /inbox\nhost: mastodon.social\ndate: #{date}\ndigest: #{digest}"
+      logger.debug "GUH!5 #{signed_string}"
       signature = Base64.strict_encode64(keypair.sign(OpenSSL::Digest::SHA256.new, signed_string))
+      logger.debug "GUH!6 #{signature}"
       header = 'keyId="' + actor_url(user.username, anchor: 'main-key') + '",headers="(request-target) host date digest",signature="' + signature + '"'
-
-      out = "POST #{inbox}\nContent-Type: application/activity+json\nDate: #{date}\nSignature: #{header}\nDigest: #{digest}\n\n#{document}"
+      logger.debug "GUH!7 #{header}"
 
       result = Excon.post(inbox, body: document, headers: { 'Content-Type': 'application/activity+json', 'Date': date, 'Signature': header, 'Digest': digest })
+      logger.debug "GUH!8 #{result.status} #{result.body}"
     end
 
     render content_type: 'application/jrd+json', json: JSON.parse(result.body)
