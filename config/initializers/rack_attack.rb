31d8fbfe2cb2f7d39f885841571816159c41ddda
Pup Gray <pupgray@outlook.com>
2024-07-21 19:04:20 -0400

    Rack attack, we have some people pinging links req/<1ms

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+class Rack::Attack
+  throttle('req/ip', limit: 80, period: 1.minute) do |req|
+    req.ip unless req.path.start_with?('/assets')
+  end
+
+  throttle('logins/ip', limit: 5, period: 20.seconds) do |req|
+    if req.path == '/session' && req.post?
+      req.ip
+    end
+  end
+end
