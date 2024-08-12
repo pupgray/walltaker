0d510cb83f7190def0ce45a525b972b3d8687881
Pup Gray <pupgray@outlook.com>
2024-08-11 21:28:29 -0400

    Better puma metrics

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,4 @@
+require 'barnes'
 # This configuration file will be evaluated by Puma. The top-level methods that
 # are invoked here are part of Puma's configuration DSL. For more information
 # about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
@@ -24,6 +25,10 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
 workers ENV.fetch("WEB_CONCURRENCY", 2)
+
+before_fork do
+  Barnes.start
+end
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
