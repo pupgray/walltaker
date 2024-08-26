9a6b245ff6d110cbf44f957c338ed560120abf36
Pup Gray <pupgray@outlook.com>
2024-08-25 20:03:36 -0400

    Lol oops

--- app/jobs/scoop_pop_job.rb
+++ app/jobs/scoop_pop_job.rb
@@ -2,7 +2,7 @@ class ScoopPopJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    next_scoop = Scoop.joins(:user).where(was_shown: true).order(created_at: :asc).first
+    next_scoop = Scoop.joins(:user).where(was_shown: false).order(created_at: :asc).first
     mutex = Rails.cache.fetch('v1/newsroom_mutex')
     if next_scoop && !mutex.present?
       Turbo::StreamsChannel.broadcast_update_to :chyron, target: :chyron, partial: 'application/chyron', locals: { message: "Breaking news from #{next_scoop.user.username}" }
