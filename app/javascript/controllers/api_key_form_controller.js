74afe648f5bf5bf0dcd6a18370efd1a8a33bb264
Pup Gray <pupgray@outlook.com>
2025-01-07 19:26:52 -0500

    Quick fix for api key form

--- app/javascript/controllers/api_key_form_controller.js
+++ app/javascript/controllers/api_key_form_controller.js
@@ -5,12 +5,14 @@ export default class ApiKeyFormController extends Controller {
     static targets = ['key']
 
     connect() {
-        if (this.keyTarget) {
+        if (this.hasKeyTarget) {
             this.keyTarget.addEventListener('click', this.copyKey.bind(this))
         }
     }
 
     copyKey() {
-        getSelection().selectAllChildren(this.keyTarget)
+        if (this.hasKeyTarget) {
+            getSelection().selectAllChildren(this.keyTarget)
+        }
     }
 }
